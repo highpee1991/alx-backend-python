95bb3f738fe35c6f1f50b25ccf853c6ba267a3b8
Ipadeola Taiwo <ipadeola.it@gmail.com>
2025-07-24 08:28:58 +0100

    feat(models): define User, Message, and Conversation models

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -1,3 +1,55 @@
+import uuid
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
+
+# Extended User model
+class User(AbstractUser):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    email = models.EmailField(unique=True, null=False)
+    phone_number = models.CharField(max_length=20, null=True, blank=True)
+
+    class Role(models.TextChoices):
+        GUEST = 'guest', _('Guest')
+        HOST = 'host', _('Host')
+        ADMIN = 'admin', _('Admin')
+
+    role = models.CharField(
+        max_length=10,
+        choices = Role.choices,
+        default= Role.GUEST,
+        null=False
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+
+    def __str__(self):
+        return f"{self.email} ({self.role})"
+    
+    
+
+# Conversation model
+class Conversation(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    participants = models.ManyToManyField(User, related_name='conversations')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Conversation {self.id}"
+    
+
+# Message model
+class Message(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='messages_sent')
+    conversation = models.ForeignKey(Conversation, on_delete=models.CASCADE, related_name='messages')
+    message_body = models.TextField(null=False)
+    sent_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Message {self.id} {self.sender.email}"
