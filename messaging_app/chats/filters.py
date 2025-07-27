a44ddb710f9386aa96b8f04004f24fbf003fe36a
Ipadeola Taiwo <ipadeola.it@gmail.com>
2025-07-27 03:34:30 +0100

    Fix pagination and filtering checks: added paginator count, PageNumberPagination, and MessageFilter integration

--- messaging_app/chats/filters.py
+++ messaging_app/chats/filters.py
@@ -1,16 +1,13 @@
+# chats/filters.py
 import django_filters
 from .models import Message
-from django.contrib.auth import get_user_model
-
-User = get_user_model
+from django.contrib.auth.models import User
 
 class MessageFilter(django_filters.FilterSet):
     sender = django_filters.ModelChoiceFilter(queryset=User.objects.all())
-    conversation = django_filters.NumberFilter(field_name='conversation__id')
-    created_after = django_filters.DateTimeFilter(field_name="created_at", lookup_expr="gte")
-    created_before = django_filters.DateTimeFilter(field_name="created_at", lookup_expr="lte")
+    start_date = django_filters.DateFilter(field_name='timestamp', lookup_expr='gte')
+    end_date = django_filters.DateFilter(field_name='timestamp', lookup_expr='lte')
 
-
-class Meta:
-    model = Message
-    fields = ['sender', 'conversation', 'created_after', 'created_before']
+    class Meta:
+        model = Message
+        fields = ['sender', 'start_date', 'end_date']
