464050da926be378457f5266c04d25e6615923d2
Ipadeola Taiwo <ipadeola.it@gmail.com>
2025-08-03 22:48:15 +0100

    Enforce chat user Role Permissions

--- Django-Middleware-0x03/chats/middleware.py
+++ Django-Middleware-0x03/chats/middleware.py
@@ -76,7 +76,7 @@ class OffensiveLanguageMiddleware:
 
 
 
-class RolePermissionMiddleware:
+class RolepermissionMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
 
