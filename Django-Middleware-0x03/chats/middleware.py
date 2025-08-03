4a46ee8e0f66eb11cf11eed803791dfd9a6adbdc
Ipadeola Taiwo <ipadeola.it@gmail.com>
2025-08-03 22:11:34 +0100

    Restrict Chat Access by time

--- Django-Middleware-0x03/chats/middleware.py
+++ Django-Middleware-0x03/chats/middleware.py
@@ -1,28 +1,5 @@
-# import logging
-# from datetime import datetime
-
-# #configure logger
-# logger = logging.getLogger(__name__)
-# handler = logging.FileHandler('user_requests.log') #log file at the root of the folder 
-# formatter = logging.Formatter('%(message)s')
-# handler.setFormatter(formatter)
-# logger.addHandler(handler)
-# logger.setLevel(logging.INFO)
-
-# class RequestLoggingMiddleware:
-#     def __init__(self, get_response):
-#         self.get_response = get_response
-
-#     def __call__(self, request):
-#         user = request.user if request.user.is_authenticated else "Anonymous"
-#         log_message = f'{datetime.now()} - User: {user} - Path: {request.path}'
-#         logger.info(log_message)
-
-#         response = self.get_response(request)
-#         return response
-
-# chats/middleware.py
-from datetime import datetime
+from datetime import datetime, time
+from django.http import HttpResponseForbidden
 
 class RequestLoggingMiddleware:
     def __init__(self, get_response):
@@ -37,3 +14,22 @@ class RequestLoggingMiddleware:
         
         response = self.get_response(request)
         return response
+
+
+
+
+class RestrictAccessByTimeMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        now = datetime.now().time()
+        allowed_start = time(18, 0) #6pm
+        allowed_end = time(21, 0) #9pm
+
+        if not (allowed_start <= now <= allowed_end):
+            # Only restrict paths starting with /chat or similar, to avoid blocking admin etc.
+            if request.path.startswith('/chat'):
+                return HttpResponseForbidden("Access to the chat is only allowed between 6PM to 9PM.")
+            
+        return self.get_response(request)
